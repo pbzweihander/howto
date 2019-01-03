2a1c23d0246569fe68cdeba3d15443f4299f112b
pbzweihander <pbzweihander@gmail.com>
2019-01-03 12:02:57 +0900

    update to 2018 edition

--- howto-cli/src/main.rs
+++ howto-cli/src/main.rs
@@ -1,19 +1,14 @@
-extern crate getopts;
-extern crate howto;
-extern crate openssl_probe;
+use {futures::prelude::*, getopts::Options, howto::*, openssl_probe, std::env, tokio};
 
-use getopts::Options;
-use howto::*;
-
-const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Debug)]
 struct Config {
     query: String,
-    position: usize,
+    position: u64,
     show_link: bool,
     show_full: bool,
-    num_answers: usize,
+    num_answers: u64,
 }
 
 fn help_message(program: &str, opts: &Options) -> String {
@@ -24,7 +19,7 @@ fn help_message(program: &str, opts: &Options) -> String {
     opts.usage(&brief)
 }
 
-fn get_config_from_args(args: Vec<String>) -> Result<Config, String> {
+fn get_config_from_args(args: &[String]) -> Result<Config, String> {
     let mut opts = Options::new();
     let program = args[0].clone();
 
@@ -34,14 +29,16 @@ fn get_config_from_args(args: Vec<String>) -> Result<Config, String> {
             "pos",
             "select answer in specified position (default: 1)",
             "POS",
-        ).optflag("a", "all", "display the full text of the answer")
+        )
+        .optflag("a", "all", "display the full text of the answer")
         .optflag("l", "link", "display only the answer link")
         .optopt(
             "n",
             "num-answers",
             "number of answers to return (default: 1)",
             "NUM_ANSWERS",
-        ).optflag("v", "version", "print the current version");
+        )
+        .optflag("v", "version", "print the current version");
 
     macro_rules! bail {
         () => {
@@ -98,9 +95,12 @@ fn get_config_from_args(args: Vec<String>) -> Result<Config, String> {
 }
 
 fn main() {
+    use futures::future::ok;
+
     openssl_probe::init_ssl_cert_env_vars();
 
-    let config = get_config_from_args(std::env::args().collect::<Vec<_>>());
+    let args = env::args().collect::<Vec<_>>();
+    let config = get_config_from_args(&args);
     if let Err(e) = config {
         println!("{}", e);
         return;
@@ -109,24 +109,30 @@ fn main() {
 
     let answers = howto(&config.query);
 
-    answers
+    let fut = answers
         .skip(config.position - 1)
         .take(config.num_answers)
-        .for_each(|answer| match answer {
-            Err(e) => eprintln!("{}\n{}", e, e.find_root_cause()),
-            Ok(answer) => {
-                if config.show_link {
-                    println!("{}", answer.link);
-                } else {
-                    if config.num_answers > 1 {
-                        println!("==== Answer from {} ====", answer.link);
-                    }
-                    if config.show_full {
-                        println!("{}\n", answer.full_text);
+        .then(ok)
+        .for_each(move |answer| {
+            match answer {
+                Err(e) => eprintln!("{}\n{}", e, e.find_root_cause()),
+                Ok(answer) => {
+                    if config.show_link {
+                        println!("{}", answer.link);
                     } else {
-                        println!("{}", answer.instruction);
+                        if config.num_answers > 1 {
+                            println!("==== Answer from {} ====", answer.link);
+                        }
+                        if config.show_full {
+                            println!("{}\n", answer.full_text);
+                        } else {
+                            println!("{}", answer.instruction);
+                        }
                     }
                 }
-            }
+            };
+            ok(())
         });
+
+    tokio::run(fut);
 }
